5bd0b4e12f8d7bb94e7706464c0bc230435c8140
codetea <64843061+codetea-ping999@users.noreply.github.com>
2026-08-11 03:50:28 +0900

    chore: connect app to Supabase project

---
+++ config.js
@@ -0,0 +1,5 @@
+window.ARTIST_PORTAL_CONFIG = {
+  supabaseUrl: "https://gvrzchxbnnxgaysadqdd.supabase.co",
+  supabasePublishableKey: "sb_publishable_kdqmSLh7k6w3FVDmc876zA_zcc9oz9g",
+  supabaseJsVersion: "2.111.0"
+};
